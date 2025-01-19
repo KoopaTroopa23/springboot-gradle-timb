09ae3864271a7250a6ae5ee33b61a62c35330f36
Kevin <57780570+KoopaTroopa23@users.noreply.github.com>
2025-01-18 19:21:18 -0500

    Initial Commit

---
+++ src/main/java/section3to10/FirstLessons.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package section3to10;
+
+/**
+ * 
+ */
+public class FirstLessons {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
